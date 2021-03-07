268b458d816763c2ee3deff5aebf8c4cc38e72d2
eraysurmeli <surmelimehmeteray@gmail.com>
2021-03-07 21:38:43 +0300

    upload system updated

--- app.py
+++ app.py
@@ -1,19 +1,79 @@
-from flask import Flask,request
+from flask import Flask, request
 import prediction
+import os
+import warnings
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from flask import Flask, flash, request, redirect, render_template
+
+PEOPLE_FOLDER = os.path.join('static')
+ALLOWED_EXTENSIONS = {'excel', 'csv', 'txt'}
+
+warnings.filterwarnings('ignore')
 
 app = Flask(__name__)
+
+app.config['UPLOAD_FOLDER'] = PEOPLE_FOLDER
+
+app = Flask(__name__)
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        return #do_the_login()
+
+@app.route('/show', methods=['GET', 'POST'])
+def show():
+    if request.method == 'GET':
+        prediction.forecastwithoption("AAPL", 30)
+        return  # do_the_login()
     else:
-        return #show_the_login_form()
+        return  # show_the_login_form()
+
+
+#
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    global dataframe
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            dataframe = pd.read_csv(request.files['file'], header=0, index_col=0)
+            file = request.files['file']
+            filename = file.filename
+            print('{} isimli dosya yuklendi'.format(filename))
+            print(dataframe)
+            dataframe = dataframe.dropna()
+            prediction.forecastwithuploadcsv(dataframe, 30)
+            # FOR PDF
+            # rendered = render_template('index.html', result=fc_series, user_image=full_filename)
+            # pdf = pdfkit.from_string(rendered,False)
+            # response = make_response(pdf)
+            # response.headers['Content-Type'] = 'application/pdf'
+            # response.headers['Content-Disposition'] = 'attachment; filename=output.pdf'
+            # return response
+
+
+def main():
+    app.run(debug=True)
+
 
 if __name__ == '__main__':
-    app.run()
+    main()
--- prediction.py
+++ prediction.py
@@ -15,48 +15,53 @@ import yfinance as yf
 warnings.filterwarnings('ignore')
 import seaborn as sns
 import itertools
+
 plt.style.use('fivethirtyeight')
 
 matplotlib.rcParams['axes.labelsize'] = 14
 matplotlib.rcParams['xtick.labelsize'] = 12
 matplotlib.rcParams['ytick.labelsize'] = 12
-compName = 'MSFT'
 
-def forecast(compName):
 
-    msft = yf.Ticker(compName)
+def forecastwithoption(compName, day):
 
+    comp = yf.Ticker(compName)
     # get historical market data
-    hist = msft.history(period="max")
-    print(hist)
-
-
-    # df = pd.read_csv('/Users/mehmeteraysurmeli/Downloads/AAPL.csv')
-    # print(df.isnull().sum())
-    df = hist
+    df = comp.history(period="max")
     df.index = pd.to_datetime(df.index)
 
     df_groupby = df.groupby(['Date'])['Close'].mean()
     df_groupby.plot(figsize=(18, 8), title='Closing Prices in Month')
-    plt.show()
+    # plt.show()
 
     df_groupby.sort_index(inplace=True)
 
     y = df_groupby
+    y = y.tail(110)
+    print(y)
+    lastDayOfDf = y.index.max().strftime("%m/%d/%Y")
+    firstDayOfDf = y.index.min().strftime("%m/%d/%Y")
+    print("first day: " + firstDayOfDf + ", Last day: " + lastDayOfDf)
+
+    """
+    onemonthlater = pd.date_range(y.index.max(), periods=30, freq='1D')
+    threemonthlater = pd.date_range(y.index.max(), periods=90, freq='1D')
+    sixmonthlater = pd.date_range(y.index.max(), periods=180, freq='1D')
+    """
 
     result_add = seasonal_decompose(x=y, model='additive', extrapolate_trend='freq', period=1)
     plt.rcParams.update({'figure.figsize': (18, 10)})
     result_add.plot().suptitle('Additive Decompose', fontsize=22)
-    plt.show()
+    # plt.show()
 
     p = d = q = range(0, 2)
     pdq = list(itertools.product(p, d, q))
     seasonal_pdq = [(x[0], x[1], x[2], 12) for x in list(itertools.product(p, d, q))]
-    print('Examples of parameter for SARIMA...')
-    print('SARIMAX: {} x {}'.format(pdq[1], seasonal_pdq[1]))
-    print('SARIMAX: {} x {}'.format(pdq[1], seasonal_pdq[2]))
-    print('SARIMAX: {} x {}'.format(pdq[2], seasonal_pdq[3]))
-    print('SARIMAX: {} x {}'.format(pdq[2], seasonal_pdq[4]))
+    # print('Examples of parameter for SARIMA...')
+    # print('SARIMAX: {} x {}'.format(pdq[1], seasonal_pdq[1]))
+    # print('SARIMAX: {} x {}'.format(pdq[1], seasonal_pdq[2]))
+    # print('SARIMAX: {} x {}'.format(pdq[2], seasonal_pdq[3]))
+    # print('SARIMAX: {} x {}'.format(pdq[2], seasonal_pdq[4]))
 
     for param in pdq:
         for param_seasonal in seasonal_pdq:
@@ -64,7 +69,7 @@ def forecast(compName):
                 mod = sm.tsa.statespace.SARIMAX(y, order=param, seasonal_order=param_seasonal,
                                                 enforce_stationarity=False, enforce_invertibility=False)
                 results = mod.fit()
-                print('ARIMA{}x{}12 - AIC:{}'.format(param, param_seasonal, results.aic))
+                # print('ARIMA{}x{}12 - AIC:{}'.format(param, param_seasonal, results.aic))
             except:
                 continue
 
@@ -79,9 +84,12 @@ def forecast(compName):
     results.plot_diagnostics(figsize=(18, 8))
     plt.show()
 
-    pred = results.get_prediction(start=pd.to_datetime('2020-01-02'), dynamic=False)
+    pred = results.get_prediction(start=pd.to_datetime(firstDayOfDf), end=pd.to_datetime(lastDayOfDf), dynamic=False)
     pred_ci = pred.conf_int()
+
+    """
     ax = y['2019-12-01':].plot(label='observed')
+
     pred.predicted_mean.plot(ax=ax, label='One-step ahead Forecast', alpha=.7, figsize=(14, 4))
     ax.fill_between(pred_ci.index,
                     pred_ci.iloc[:, 0],
@@ -90,6 +98,7 @@ def forecast(compName):
     ax.set_ylabel('Sold')
     plt.legend()
     plt.show()
+    """
 
     print(pred_ci)
 
@@ -99,9 +108,11 @@ def forecast(compName):
     print('The Mean Squared Error is {}'.format(round(mse, 2)))
     print('The Root Mean Squared Error is {}'.format(round(np.sqrt(mse), 2)))
 
-    pred_uc = results.get_forecast(steps=12)
+    pred_uc = results.get_forecast(steps=90)
     pred_ci = pred_uc.conf_int()
+
     ax = y.plot(label='observed', figsize=(14, 4))
+
     pred_uc.predicted_mean.plot(ax=ax, label='Forecast')
     ax.fill_between(pred_ci.index,
                     pred_ci.iloc[:, 0],
@@ -112,10 +123,116 @@ def forecast(compName):
     plt.show()
 
     y_forecasted = pred.predicted_mean
-    y_forecasted.head(12)
+    y_forecasted.head(day)
 
     forecast = pred_uc.predicted_mean
-    print(forecast.head(12))
+    print(forecast.head(day))
 
-#forecast(compName)
+    print(pred)
+    return y_forecasted.head(day)
 
+
+def forecastwithuploadcsv(csv, day):
+    df = csv
+    df.index = pd.to_datetime(df.index)
+
+    df_groupby = df.groupby(['Date'])['Close'].mean()
+    df_groupby.plot(figsize=(18, 8), title='Closing Prices in Month')
+    # plt.show()
+
+    df_groupby.sort_index(inplace=True)
+
+    y = df_groupby
+    y = y.tail(110)
+    print(y)
+    lastDayOfDf = y.index.max().strftime("%m/%d/%Y")
+    firstDayOfDf = y.index.min().strftime("%m/%d/%Y")
+    print("first day: " + firstDayOfDf + ", Last day: " + lastDayOfDf)
+
+    """
+    onemonthlater = pd.date_range(y.index.max(), periods=30, freq='1D')
+    threemonthlater = pd.date_range(y.index.max(), periods=90, freq='1D')
+    sixmonthlater = pd.date_range(y.index.max(), periods=180, freq='1D')
+    """
+
+    result_add = seasonal_decompose(x=y, model='additive', extrapolate_trend='freq', period=1)
+    plt.rcParams.update({'figure.figsize': (18, 10)})
+    result_add.plot().suptitle('Additive Decompose', fontsize=22)
+    # plt.show()
+
+    p = d = q = range(0, 2)
+    pdq = list(itertools.product(p, d, q))
+    seasonal_pdq = [(x[0], x[1], x[2], 12) for x in list(itertools.product(p, d, q))]
+    # print('Examples of parameter for SARIMA...')
+    # print('SARIMAX: {} x {}'.format(pdq[1], seasonal_pdq[1]))
+    # print('SARIMAX: {} x {}'.format(pdq[1], seasonal_pdq[2]))
+    # print('SARIMAX: {} x {}'.format(pdq[2], seasonal_pdq[3]))
+    # print('SARIMAX: {} x {}'.format(pdq[2], seasonal_pdq[4]))
+
+    for param in pdq:
+        for param_seasonal in seasonal_pdq:
+            try:
+                mod = sm.tsa.statespace.SARIMAX(y, order=param, seasonal_order=param_seasonal,
+                                                enforce_stationarity=False, enforce_invertibility=False)
+                results = mod.fit()
+                # print('ARIMA{}x{}12 - AIC:{}'.format(param, param_seasonal, results.aic))
+            except:
+                continue
+
+    mod = sm.tsa.statespace.SARIMAX(y,
+                                    order=(0, 0, 1),
+                                    seasonal_order=(1, 1, 1, 12),
+                                    enforce_stationarity=False,
+                                    enforce_invertibility=False)
+    results = mod.fit()
+    results.summary()
+
+    results.plot_diagnostics(figsize=(18, 8))
+    plt.show()
+
+    pred = results.get_prediction(start=pd.to_datetime(firstDayOfDf), end=pd.to_datetime(lastDayOfDf), dynamic=False)
+    pred_ci = pred.conf_int()
+
+    """
+    ax = y['2019-12-01':].plot(label='observed')
+
+    pred.predicted_mean.plot(ax=ax, label='One-step ahead Forecast', alpha=.7, figsize=(14, 4))
+    ax.fill_between(pred_ci.index,
+                    pred_ci.iloc[:, 0],
+                    pred_ci.iloc[:, 1], color='k', alpha=.2)
+    ax.set_xlabel('Date')
+    ax.set_ylabel('Sold')
+    plt.legend()
+    plt.show()
+    """
+
+    print(pred_ci)
+
+    y_forecasted = pred.predicted_mean
+    y_truth = y['2018-06-01':]
+    mse = ((y_forecasted - y_truth) ** 2).mean()
+    print('The Mean Squared Error is {}'.format(round(mse, 2)))
+    print('The Root Mean Squared Error is {}'.format(round(np.sqrt(mse), 2)))
+
+    pred_uc = results.get_forecast(steps=90)
+    pred_ci = pred_uc.conf_int()
+
+    ax = y.plot(label='observed', figsize=(14, 4))
+
+    pred_uc.predicted_mean.plot(ax=ax, label='Forecast')
+    ax.fill_between(pred_ci.index,
+                    pred_ci.iloc[:, 0],
+                    pred_ci.iloc[:, 1], color='k', alpha=.25)
+    ax.set_xlabel('Date')
+    ax.set_ylabel('Sales')
+    plt.legend()
+    plt.show()
+
+    y_forecasted = pred.predicted_mean
+    y_forecasted.head(day)
+
+    forecast = pred_uc.predicted_mean
+    print(forecast.head(day))
+
+    print(pred)
+    return y_forecasted.head(day)
